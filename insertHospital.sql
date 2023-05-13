35c94e89f4b344f24945c987390951caac57d155
Alinny Ribeiro <alinnyrc6@gmail.com>
2023-05-12 22:43:43 -0300

    Mais inserções, faltam apenas 5 itens
    Agora todos os dados de pacientes e médicos são reais, além das consultas e receitas que batem de acordo com a especialidade do médico, valores, remédios e instruções sobre como tomá-los

--- insertHospital.sql
+++ insertHospital.sql
@@ -1,36 +1,29 @@
 use hospital;
-show tables;
-show create table medico;
 
-select * from medico;
+insert into crm values 
+(null, '47742', DATE '1984-02-21','SP'),
+(null, '168120', DATE '2014-11-11','SP'),
+(null, '101092', DATE '2000-12-19','SP'),
+(null, '118282 ', DATE '2005-02-15','SP'),
+(null, '178899', DATE '2016-03-08','SP'),
+(null, '116106', DATE '2004-12-22','SP'),
+(null, '123776 ', DATE '2006-07-04','SP'),
+(null, '109135 ', DATE '2003-01-14','SP'),
+(null, '156128', DATE '2012-12-11','SP'),
+(null, '166420', DATE '2018-03-06','SP');
+
 insert into medico values
 (null, '5684751125', 'Newton Nakamura', curdate(), 1, '+5520925999'),
 (null, '5561488139', 'Camila Maria Barbosa', curdate(), 2, '+551134767254'),
 (null, '3654127003', 'Juliana Mossini Nicoliello', curdate(), 3, '+551150838222'),
 (null, '7854544774', 'Sergio Belczak', curdate(), 4, '+551138925642'),
 (null, '8547851126', 'Andressa Amorim', curdate(), 5, '+551150524527'),
+(null, '0089682132', 'Polo Eduardo San Martin Gonzales', curdate(), 6, '+551127378044'),
+(null, '7458126948', 'João Vitorino', curdate(), 7, '+5511950702845'),
+(null, '8544545153', 'Thiago Giansante Abud', curdate(), 8, '+551156886100'),
+(null, '9987455444', 'Heli Samuel Pinto Souza', curdate(), 9, '+551121518709'),
+(null, '7754841688', 'Janaina Petenuci', curdate(), 10, '+551155498740');
 
-(null, '0089682132', '', curdate(), 5, '+5599945871256'),
-(null, '7458126948', '', curdate(), 6, '+5511995143677'),
-(null, '8544545153', '', curdate(), 7, '+5515985475412'),
-(null, '9987455444', '', curdate(), 9, '+5511996541274'),
-(null, '7754841688', '', curdate(), 10, '+5548965444214');
-
-select * from crm;
-insert into crm values 
-(null, '47742', curdate(),'SP'),
-(null, '168120', curdate(),'SP'),
-(null, '101092', curdate(),'SP'),
-(null, '118282 ', curdate(),'SP'),
-(null, '178899', curdate(),'SP'),
-
-(null, '985478521', curdate(),'SP'),
-(null, '145217478', curdate(),'SP'),
-(null, '102478521', curdate(),'CE'),
-(null, '021455248', curdate(),'SP'),
-(null, '004578512', curdate(),'SC');
-
-select * from especialidade;
 insert into especialidade values
 (null, 'Pediatria', 'PED'),
 (null, 'Clínica Geral', null),
@@ -39,11 +32,10 @@ insert into especialidade values
 (null, 'Ginecologista','gineco'),
 (null, 'Mastologia', null),
 (null, 'Oftalmologia', 'oftalmo'),
-(null, 'Radioterapia', null),
-(null, 'Homeopatia', null),
-(null, 'Cardiologia', null);
+(null, 'Radioterapia', 'radio'),
+(null, 'Neurologia', 'neuro'),
+(null, 'Cardiologia', 'cardio');
 
-select * from medicoEspecialidade;
 INSERT INTO medicoEspecialidade values
 (1, 1,DATE '1983-12-20'),
 (2, 1, DATE '2017-12-09'),
@@ -53,52 +45,90 @@ INSERT INTO medicoEspecialidade values
 (4, 10, DATE '2008-12-20'),
 (5, 5, DATE '2017-12-20'),
 (5, 6, DATE '2019-12-20'),
+(6, 7, DATE '2007-12-20'),
+(7, 9, DATE '2016-12-20'),
+(8, 8, DATE '2010-12-20'),
+(9, 2, DATE '2018-12-10'),
+(10, 2, DATE '2018-12-10');
 
-(8, 1,curdate()),
-(9, 7,curdate()),
-(10, 9,curdate()),
-(10, 8,curdate()),
-(5, 2,curdate());
+select * from medico;
+select * from crm;
+select * from especialidade;
+select * from medicoEspecialidade;
 
+select * from paciente;
 INSERT INTO  paciente values
-(null, 'Guilherme Papaléo', '9984454561', curdate(), '+5511998544210'),
-(null, 'Guilherme Maranho', '5874120046', curdate(), '+5512585455785'),
-(null, 'Amanda Silva Santos', '0468245996', curdate(), '+5515985551054'),
-(null, 'Jaqueline Guimaraes', '5874598446', curdate(), '+5578995624788'),
-(null, 'Aline Barros', '8547845461', curdate(), '+5511958769532'),
-(null, 'Laura Gomes', '8956565633', curdate(), '+5512785445542'),
-(null, 'Vitória Barberi', '0054446529', curdate(), '+5511854565665'),
-(null, 'Kaique Souza', '9658745512', curdate(), '+5511998545666'),
-(null, 'Robson Cleiton', '0025410368', curdate(), '+5511995745652'),
-(null, 'Eduardo Pestana', '9545544105', curdate(), '+5515922548781'),
-(null, 'Jefferson Professor', '9854411003', curdate(), '+5511895844550'),
-(null, 'Houdini', '9856451054', curdate(), '+5511965442059'),
-(null, 'Pablo Gomes', '8122204548', curdate(), '+5511620445445'),
-(null, 'Yasmin Santana', '9852411054', curdate(), '+5511698885089'),
-(null, 'Alex Pandrea', '8547412305', curdate(), '+5511998445120');
+(null, 'MARIA LUCIA FRANCISCO AROFO', '23110501848', DATE '1942-03-12', '+552121050000'),
+(null, 'EMERSON MENA', '88594432968', DATE '1973-05-12', '+554535244382'),
+(null, 'MARIA JOSE SILVA OLIVEIRA', '02796198472', DATE '1930-06-07', '+5535998186140'),
+(null, 'CARLOS ALEXANDRE FIGUEIRA JORGE', '07005503724', DATE '1973-10-09', '+5521978862189'),
+(null, 'REGINALDO FERREIRA LIMA', '15624504845', DATE '1975/02/17', '+5516993043521'),
+(null, 'MARIA JOSE SILVA LEITE', '04815607427', DATE '1982-03-09', '+5581992505153'),
+(null, 'ELIANA ANTONIA SILVA SANTOS', '44590210487', DATE '1965-11-09', '+5587988152415'),
+(null, 'JENIFER NAIANY RAMOS DE JESUS', '06249030522', DATE '1994-11-25', '+551123754683'),
+(null, 'VANILCE VIANA OLIVEIRA', '93253095215', DATE '2004-09-14', '+5593991636079'),
+(null, 'VINICIUS RODRIGUES LAGES', '16309082663', DATE '2000-06-16', '+5514997341488'),
+(null, 'EMILLY ROSA ALTHMAN', '49030243856', DATE '2000-08-30', '+5516997452898'),
+(null, 'HERNAN ALMEIDA DE OLIVEIRA', '00485694298', DATE '1989-05-06', '+552984241470'),
+(null, 'CINTIA FERREIRA DA SILVA', '16983932889', DATE '1975-11-16', '+5517991171099'),
+(null, 'JOAO PEREIRA DE FARIA', '30906954649', DATE '1933-05-17', '+5516997452898'),
+(null, 'ANTONIO JULIO SILVESTRE FILHO', '05027616791', DATE '1927-06-05', '+5514997106127');
+
+insert into convenio values 
+(null, 1, 'Unimed', '128192', '60 dias'),
+(null, 5, 'Plamol', '19271209', '30 dias'),
+(null, 10, 'Amil', '901725417', '60 dias'),
+(null, 15, 'Porto Seguro', '8192192', '30 dias');
 
 insert into consulta values
-(null, 'id_medico', 'id_paciente', 'dt', "hr", 'valor')
-(null, 1, 14, DATE '2015-01-02', '11:20', 'R$00,00'), //yasmin Ginecologista (grávida)
-(null, 2, 10, DATE '2016-02-02','12:30', 'R$00,00'), //Eduardo Cardiologia
-(null, 6, 1, DATE '2017-03-02','14:00', 'R$00,00'), //GUILHERME Papaléo RADIOLOGIA(QUEBROU A PERNA)
-(null, 7, 3, DATE '2018-04-02','15:25', 'R$00,00'), //AMANDA pediatra
-(null, 3, 10, DATE '2019-05-02','16:20', 'R$00,00'), //eduardo dermato
-(null, 5, 11, DATE '2020-06-02','10:50', 'R$00,00'), //jeff Gastrenterologia (gastrite) 
-(null, 1, 5, DATE '2021-07-02','11:00', 'R$00,00'), //aline Mastologia 
-(null, 7, 11, DATE '2015-08-02','14:00', 'R$00,00'), //jeff oftalmo
-(null, 4, 2, DATE '2016-09-02','10:00', 'R$00,00'), //Guilherme maranhao clinico geral
-(null, 4, 4, DATE '2017-10-02','12:20', 'R$00,00'), //jaqueline clinico geral
-(null, 3, 4, DATE '2018-11-02','09:00', 'R$00,00'), //jaqueline Dermatologia (insolação)
-(null, 10, 5, DATE '2019-12-02','8:30', 'R$00,00'), //aline barros Homeopatia (ansiedade)
-(null, 1, 6, DATE '2020-12-02','15:20', 'R$00,00'), //Laura mastologia
-(null, 8, 7, DATE '2021-01-02','08:00', 'R$00,00'), //vitoria pediatria
-(null, 9, 8, DATE '2015-02-02','12:00', 'R$00,00'), //Kaique oftalmo
-(null, 5, 9, DATE '2016-03-02','13:00', 'R$00,00'), //robson clinico
-(null, 2, 12, DATE '2017-04-02','09:00', 'R$00,00'), //CardiologiaHoudini
-(null, 5, 13, DATE '2018-05-02','16:20', 'R$00,00'), //Pablo clinico
-(null, 10, 15, DATE '2019-06-02','17:00', 'R$00,00'), //alex Homeopatia (ansioso)
-(null, 6, 10, DATE '2022-01-01','10:20', 'R$00,00'); //eduardo radiologia
+(null, 1, 9, DATE '2015-01-02', '11:20', 'R$250,00', null), 
+(null, 2, 10, DATE '2016-02-02','12:30', 'R$150,50', 3), 
+(null, 3, 3, DATE '2017-03-02','14:00', 'R$320,00', null), 
+(null, 4, 7, DATE '2018-04-02','15:25', 'R$300,00', null), 
+(null, 5, 11, DATE '2019-05-02','16:20', 'R$109,00', null), 
+(null, 6, 4, DATE '2020-06-02','10:50', 'R$200,00', null), 
+(null, 7, 5, DATE '2021-07-02','11:00', 'R$150,00', 2), 
+(null, 8, 8, DATE '2015-08-02','14:00', 'R$500,00', null), 
+(null, 9, 2, DATE '2016-09-02','10:00', 'R$60,00', null), 
+(null, 10, 11, DATE '2017-10-02','12:20', 'R$90,00', null), 
+(null, 1, 10, DATE '2011-01-02', '11:20', 'R$120,00', 3), 
+(null, 2, 1, DATE '2016-02-02','12:30', 'R$122,50', 1), 
+(null, 3, 6, DATE '2017-03-02','14:00', 'R$280,00', null), 
+(null, 4, 9, DATE '2018-04-02','15:25', 'R$170,00', null), 
+(null, 5, 13, DATE '2019-05-02','16:20', 'R$60,00', null), 
+(null, 6, 12, DATE '2020-06-02','10:50', 'R$140,00', null), 
+(null, 7, 14, DATE '2021-07-02','11:00', 'R$189,00', null), 
+(null, 8, 14, DATE '2015-08-02','14:00', 'R$500,00', null), 
+(null, 9, 2, DATE '2016-09-02','10:00', 'R$200,00', null), 
+(null, 10, 15, DATE '2017-10-02','12:20', 'R$90,00', 4);
 
-select * from medicoEspecialidade inner join medico on medicoEspecialidade.id_medicoESP=medico.id_medico;
-select * from medicoEspecialidade inner join especialidade on medicoEspecialidade.id_especialidadeESP=especialidade.id_especialidade;
+insert into receita values 
+(null, 3, 'Minoxidil, Lavitan, Cortisona', '2 caixas de cada', 'De 12 em horas: 1 comprimido de Minoxidil; Uma cápsula de Lavitan por dia; 7,5 mg de Cortisona por dia'),
+(null, 5, 'Buscofem, dipirona', 'Buscofem: 2 caixas de 20 cápsulas; Dipirona: 1 caixa', 'A cada 6 horas tomar um comprimido de Buscofem até o fim do período menstrual. Em caso de dor, tomar um comprimido de dipirona'),
+(null, 19, 'Dorflex, Cimegripe, Loratadina', '1 caixa de cada', 'Dipirona monoidratada: quando sentir enxaqueca ou tiver febre; Cimegripe: 1 cápsula a cada 4 horas; Loratadina: 1 cápsula por dia -- Por 4 dias'),
+(null, 2, 'Ambroxol, Lavitan Kids', '1 caixa de Ambroxol e Lavitam 1 caixa para cada 20 dias', 'Ambroxol: 5ml 3x por dia; Lavitan Kids: 1 comprimido por dia (tomar no mínimo por 3 meses e marcar retorno)'),
+(null, 12, 'Omeprazol, Dramin', '2 caixa de cada', 'Omeprazol e dramin: ingerir antes das refeições por 3 dias'),
+(null, 3, 'Sulfato de neomicina + bacitracina zíncica (Nebacetin); Acetato de clostebol + sulfato de neomicina', '1 caixa de cada', 'Aplicar a pomada de 2 a 5 vezes por dia na queimadura, até que a ferida cicatrize.'),
+(null, 6, 'Luteína', '1 caixa com 30 cápsulas', 'Luteína: 2x ao dia por 15 dias'),
+(null, 2, 'Floratil, Imosec', '1 caixa de cada', 'Floratil: 1 comprimido a cada 12 horas (de preferência em jejum) por 5 dias; Imosec: 2 comprimidos por dia + 1 a cada evacuação liquida até que as fezes voltem ao normal.'),
+(null, 10, 'Acetilcisteína Xarope, Prednisona', '1 caixa de cada', 'Acetilcisteína: de 2 a 3 soluções por dia; Prednisona: 1 comprimido por dia. Realizar inalações com soro fisiológico antes de dormir.'),
+(null, 9, 'Ibuprofeno, Dipirona, Paracetamol', '2 caixas de Ibuprofeno e Paracetamol + 1 caixa de Dipirona', '2 comprimidos de Ibuprofeno 2x por dia; Dipirona sempre que sentir dor; 1 paracetamol a cada 6 horas.');
+
+
+SELECT * FROM consulta
+INNER JOIN paciente ON consulta.id_pacienteCons = paciente.id_paciente
+INNER JOIN medico ON consulta.id_medicoCons = medico.id_medico
+order by consulta.id_consulta;
+
+select * from medicoEspecialidade 
+inner join medico on medicoEspecialidade.id_medicoESP=medico.id_medico
+inner join especialidade on medicoEspecialidade.id_especialidadeESP=especialidade.id_especialidade
+order by medico.id_medico;
+
+select * from receita
+inner join consulta on receita.id_consultaRec=consulta.id_consulta
+inner join medicoEspecialidade on consulta.id_medicoCons=medicoEspecialidade.id_medicoESP
+inner join especialidade on medicoEspecialidade.id_especialidadeESP=especialidade.id_especialidade;
+
+select * from consulta;
+select * from paciente;
