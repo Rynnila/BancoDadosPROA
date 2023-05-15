ce268d21751ec275f271f2a6a1ef3f2ebeac8415
Alinny Ribeiro <alinnyrc6@gmail.com>
2023-05-15 19:15:39 -0300

    Parte 4 da atividade
    Usando o alter e o update para colocar a coluna de atividade em médico

---
+++ alterHospital.sql
@@ -0,0 +1,11 @@
+use hospital;
+
+alter table medico add atividade varchar(12);
+
+update medico set atividade="em_atividade";
+
+update medico set atividade="inativo" where id_medico=1;
+
+update medico set atividade="inativo" where id_medico=2;
+
+select * from medico;
